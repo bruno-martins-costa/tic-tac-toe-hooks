4b70296970fd3d191e2ddc3bb79c4b2342058c7e
Bruno <brunomartins942@gmail.com>
2020-09-09 16:41:50 -0300

    Updates

---
+++ precache-manifest.d8e39cb57bfd53c4308dbbf27d78bac0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ef182bb8c1968d8fbb393040a3683a6",
+    "url": "/tic-tac-toe-hooks/index.html"
+  },
+  {
+    "revision": "18ed9d92742242912e78",
+    "url": "/tic-tac-toe-hooks/static/css/main.11c0e877.chunk.css"
+  },
+  {
+    "revision": "2934e9a7dacc6b67ffbb",
+    "url": "/tic-tac-toe-hooks/static/js/2.fd0ef66e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-hooks/static/js/2.fd0ef66e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ed9d92742242912e78",
+    "url": "/tic-tac-toe-hooks/static/js/main.eabcc492.chunk.js"
+  },
+  {
+    "revision": "8f657babd98781e72a06",
+    "url": "/tic-tac-toe-hooks/static/js/runtime-main.caddfb6d.js"
+  }
+]);
